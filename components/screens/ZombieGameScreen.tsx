f19d939d5dc695648d01693562e63078d18ebb10
josegabrielgar184-afk <josegabrielgar184@gmail.com>
2026-08-06 15:55:51 -0500

    Fix cleanup function for event listeners in ZombieGameScreen

--- components/screens/ZombieGameScreen.tsx
+++ components/screens/ZombieGameScreen.tsx
@@ -1377,16 +1377,17 @@ useEffect(() => {
     canvas.addEventListener('mousedown', onMouseDown);
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mouseup', onMouseUp);
-    return () => {
+ return () => {
       canvas.removeEventListener('touchstart', onTouchStart);
       canvas.removeEventListener('touchmove', onTouchMove);
       canvas.removeEventListener('touchend', onTouchEnd);
       canvas.removeEventListener('mousedown', onMouseDown);
       canvas.removeEventListener('mousemove', onMouseMove);
       canvas.removeEventListener('mouseup', onMouseUp);
+    };
+  }, []);
 
   return (
-    <div className="absolute inset-0 bg-black flex flex-col">
       <OfflineBanner />
       <canvas ref={canvasRef} className="absolute inset-0 w-full h-full" />
       <div className="absolute top-0 left-0 right-0 z-10 flex items-center justify-between px-4 pt-3 pb-2">
