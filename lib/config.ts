ede8faf4d013115925705d120b10b0dfed00a601
josegabrielgar184-afk <josegabrielgar184@gmail.com>
2026-09-20 07:54:38 -0500

    Update APP_VERSION from 2 to 54

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 export const MODO_PRUEBA = false;
 
-export const APP_VERSION = 2;
+export const APP_VERSION = 54;
 export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.garricraft.garrdash';
 
 export const VIP_DISPONIBLE_PLAYSTORE = false;
